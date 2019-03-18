98dea10450acd40660269fedc1684ba047eaa4be
vignesharulmani <32646209+vignesharulmani@users.noreply.github.com>
2019-03-18 17:20:57 +0530

    Mode value changes
    Mode value has been changed for individual posted items.

--- 226.dbo.fn_calc_runtime_adtype.UserDefinedFunction.sql
+++ 226.dbo.fn_calc_runtime_adtype.UserDefinedFunction.sql
@@ -1,4 +1,4 @@
-ALTER function fn_calc_runtime_adtype(@admode smallint)
+Alter function fn_calc_runtime_adtype(@admode smallint)
 returns varchar(32)
 as
 begin
@@ -6,17 +6,17 @@ begin
 declare @returnvalue varchar(32) = ''
 
 
-if @admode = 102
+if @admode = 110
 	set @returnvalue = 'Premium'
-else if @admode = 103
-	set @returnvalue = 'Priority'
 else if @admode = 105
-	set @returnvalue = '' /*Free*/
+	set @returnvalue = 'Priority'
+else if @admode = 100
+	set @returnvalue = ''/*Free*/
 else if @admode = 200
 	set @returnvalue = 'Titanium'
 else if @admode in (199,198,197)
 	set @returnvalue = 'Platinum'
-else  
+else 
 	set @returnvalue = 'Sponsored'
 
 return (@returnvalue)
