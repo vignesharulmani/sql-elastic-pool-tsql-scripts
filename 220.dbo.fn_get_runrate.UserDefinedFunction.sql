47c1361f6d3161f174910960215a731137de84b6
vignesharulmani <32646209+vignesharulmani@users.noreply.github.com>
2019-05-14 09:03:58 +0530

    Bug Fixes
    Some fixes has been applied for below functionalities
    
    Runrate Calculation
    Free ad post eligibility
    Rescommendation Logic
    Ad Extension

--- 220.dbo.fn_get_runrate.UserDefinedFunction.sql
+++ 220.dbo.fn_get_runrate.UserDefinedFunction.sql
@@ -1,4 +1,4 @@
-create function fn_get_runrate(@businessid int,@cityid int,@customerid int,@campaignid int)
+Alter function fn_get_runrate(@businessid int,@cityid int,@customerid int,@campaignid int)
 returns float
 as
 begin
@@ -6,10 +6,8 @@ begin
 declare @returnvalue float = 0
 
 select top 1 @returnvalue = runrate from campaignpriority cp (nolock)
-where (cp.businessid = @businessid or @businessid =0)
-and cp.cityid = @cityid
-and (cp.customerid = @customerid or @customerid = 0)
-and (cp.campaignid = @campaignid or @campaignid = 0)
+where cp.cityid = @cityid
+and (cp.campaignid = @campaignid)
 and cp.status = 1
 
 return (isnull(nullif(@returnvalue,''),0))
